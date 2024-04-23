a2f80215e4c39a91703d36282ea255f23a6f4d86
dspodina <spodina.darya@gmail.com>
2024-04-23 10:44:56 +0200

    unit testing completed

--- src/utils/sortFlight.js
+++ src/utils/sortFlight.js
@@ -1,3 +1,9 @@
+/**
+ * Sort the flights by departure date
+ * @param {Object} data - the data object
+ * @returns {Array} - the sorted flights
+ */
+
 const sortFlights = (data) => {
     const sortedFlights = data.flights.sort((a, b) => {
         return (
